f4e681325352cac10ead38e44a3dc0b85903002e
Edmund Kong <edmundkong9259@gmail.com>
2024-02-10 13:06:47 +0800

    Complete

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Fruit {
     private int x;
@@ -22,4 +23,27 @@ public class Fruit {
         g.fillOval(this.x, this.y, Main.CELL_SIZE, Main.CELL_SIZE);
     }
 
+    public void setNewLocation(Snake s) {
+        int new_x;
+        int new_y;
+        boolean overlapping;
+        do {
+            new_x = (int)(Math.floor(Math.random() * Main.column) * Main.CELL_SIZE);
+            new_y = (int)(Math.floor(Math.random() * Main.row) * Main.CELL_SIZE);
+            overlapping = check_overlap(new_x, new_y, s);
+        } while(overlapping);
+
+        this.x = new_y;
+        this.y = new_y;
+    }
+
+    private boolean check_overlap(int x, int y, Snake s) {
+        ArrayList<Node> snake_body = s.getSnakeBody();
+        for (int j = 0; j < s.getSnakeBody().size(); j++){
+            if (x == snake_body.get(j).x && y == snake_body.get(j).y) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
